ba1c07fdd320b8e3cea49c4106934b3783cb3591
Taylor Frost <taylorhamblinfrost@gmail.com>
2016-07-27 14:35:17 -0600

    Committed to the app store

--- BirthdayCard/BirthdayCard/SecondViewController.swift
+++ BirthdayCard/BirthdayCard/SecondViewController.swift
@@ -24,6 +24,7 @@ class SecondViewController: UIViewController, UINavigationControllerDelegate, UI
         super.viewDidLoad()
         
         self.imagePicker.delegate = self
+
         
     }
     
